7e03c78d54222b1e58dd9786ff88b859eba59bed
Zhiying Fan <fanzhiying1992@gmail.com>
2021-07-10 12:50:29 +0800

    Initial Commit

---
+++ SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by Zhiying Fan on 2021/7/10.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
